e6c101280005de129d9a58b312035fe2280851db
manik <maniksarker652@gmail.com>
2025-05-14 09:16:08 +0600

    make changes in subscription validation schema

--- src/app/modules/subscription/subscription.validation.ts
+++ src/app/modules/subscription/subscription.validation.ts
@@ -2,14 +2,28 @@ import { z } from 'zod';
 
 const createSubscriptionValidationSchema = z.object({
     body: z.object({
-        type: z.string().min(1, 'Type is required'),
-        price: z.number().min(0, 'Price must be a positive number'),
-        description: z.string().min(1, 'Description is required'),
+        type: z
+            .string({ required_error: 'Subscription type is required' })
+            .min(1, 'Type is required'),
+        price: z
+            .number({ required_error: 'Price is required' })
+            .min(0, 'Price must be a positive number'),
+        description: z
+            .string({ required_error: 'Description is required' })
+            .min(1, 'Description is required'),
+    }),
+});
+const updateSubscriptionValidationSchema = z.object({
+    body: z.object({
+        type: z.string().optional(),
+        price: z.number().optional(),
+        description: z.string().optional(),
     }),
 });
 
 const SubscriptionValidations = {
     createSubscriptionValidationSchema,
+    updateSubscriptionValidationSchema,
 };
 
 export default SubscriptionValidations;
